8692ef0792cb65944e1124559981d3359aebb36b
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-04-16 12:48:24 +0100

    feat: clase del restaurante

--- src/com/salesianos/actividad/Restaurante.java
+++ src/com/salesianos/actividad/Restaurante.java
@@ -1,4 +1,49 @@
 package com.salesianos.actividad;
 
 public class Restaurante {
+    private String name;
+    private String place;
+    private String schedule;
+    private double score;
+
+    public Restaurante(String name, String place, String schedule, double score) {
+        this.name = name;
+        this.place = place;
+        this.schedule = schedule;
+        this.score = score;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlace() {
+        return this.place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getSchedule() {
+        return this.schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+
+    public double getScore() {
+        return this.score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    
 }
