6b1d4d43d9dde37abd90fd58e99f2d61885633f8
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-04-17 22:05:09 +0100

    feat: editar restaurante

--- src/com/salesianos/actividad/menu.java
+++ src/com/salesianos/actividad/menu.java
@@ -2,11 +2,28 @@ package com.salesianos.actividad;
 
 import java.util.ArrayList;
 
-public class menu {
+import javax.swing.JOptionPane;
+
+public class Menu {
     private static final ArrayList<Restaurante> RESTAURANTS = new ArrayList<>();
 
     public static void addRestaurant(Restaurante newRestaurant){
         RESTAURANTS.add(newRestaurant);
     }
     
+    public static void modifyRestaurant(String name) {
+        for (Restaurante restaurant : RESTAURANTS) {
+            if (restaurant.getName().equalsIgnoreCase(name)) {
+                String newName = JOptionPane.showInputDialog("Introduzca el nuevo nombre del restaurante:");
+                restaurant.setName(newName);
+                String newPlace = JOptionPane.showInputDialog("Introduce el lugar dónde ahora está este restaurante:");
+                restaurant.setPlace(newPlace);
+                String newSchedule = JOptionPane.showInputDialog("Introduce el nuevo horario del restaurante:");
+                restaurant.setSchedule(newSchedule);
+                String score = JOptionPane.showInputDialog("Introduce la nueva puntuación que se le ha dado al restaurante");
+                double realScore = Integer.parseInt(score); 
+                restaurant.setScore(realScore);               
+            }
+        }
+    }
 }
