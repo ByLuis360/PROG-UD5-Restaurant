4eba73e1025f48c78552d9d52b5a6aec142d9c69
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-04-19 11:44:57 +0100

    feat: bucle y menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
+import javax.swing.JOptionPane;
 
 public class App {
-
+public static void main(String[] args) {
+    
+    String menu = """
+            1.- Añadir restaurante
+            2.- Editar restaurante
+            3.- Mostrar restaurantes
+            4.- Eliminar restaurante
+            Q.- Salir del programa
+            """;
+    boolean keepGoing = true;
+    String option;
+    while (keepGoing) {
+        option = JOptionPane.showInputDialog(menu);
+        switch (option) {
+            case "1":
+                
+                break;
+            case "2":
+                
+                break;
+            case "3":
+                
+                break;
+            case "4":
+                
+                break;
+            case "Q":
+                
+                break;
+        
+            default:
+                break;
+        }
+    }    
+}
     
 }
