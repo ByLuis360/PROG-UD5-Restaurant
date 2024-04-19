b68d7ba37e29675eec4d6aa2440ef8823e3a6b92
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-04-19 12:30:11 +0100

    feat: opcion 2: modificar restaurante

--- src/App.java
+++ src/App.java
@@ -4,41 +4,42 @@ import com.salesianos.actividad.Menu;
 import com.salesianos.actividad.Restaurante;
 
 public class App {
-public static void main(String[] args) {
-    
-    String menu = """
-            1.- Añadir restaurante
-            2.- Editar restaurante
-            3.- Mostrar restaurantes
-            4.- Eliminar restaurante
-            Q.- Salir del programa
-            """;
-    boolean keepGoing = true;
-    String option;
-    while (keepGoing) {
-        option = JOptionPane.showInputDialog(menu);
-        switch (option) {
-            case "1":
-                Restaurante newRestaurant = Menu.setInfoToNewRestaurant();
-                Menu.addRestaurant(newRestaurant);
-                break;
-            case "2":
-                
-                break;
-            case "3":
-                
-                break;
-            case "4":
-                
-                break;
-            case "Q":
-                
-                break;
-        
-            default:
-                break;
+    public static void main(String[] args) {
+
+        String menu = """
+                1.- Añadir restaurante
+                2.- Editar restaurante
+                3.- Mostrar restaurantes
+                4.- Eliminar restaurante
+                Q.- Salir del programa
+                """;
+        boolean keepAsking = true;
+        String option;
+        while (keepAsking) {
+            option = JOptionPane.showInputDialog(menu);
+            switch (option) {
+                case "1":
+                    Restaurante newRestaurant = Menu.setInfoToNewRestaurant();
+                    Menu.addRestaurant(newRestaurant);
+                    break;
+                case "2":
+                    String name = JOptionPane.showInputDialog("Introduzca el nombre del restaurante a modificar: ");
+                    Menu.modifyRestaurant(name);
+                    break;
+                case "3":
+
+                    break;
+                case "4":
+
+                    break;
+                case "Q":
+
+                    break;
+
+                default:
+                    break;
+            }
         }
-    }    
-}
-    
+    }
+
 }
